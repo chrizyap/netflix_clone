585c24b125d4f8ab27ddc774582a319b18a392fa
Christopher <christopher.adeogun@gmail.com>
2021-06-16 13:06:05 +0100

    remove debugg banner

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: Home(title: 'Flutter Demo Home Page'),
     );
   }
